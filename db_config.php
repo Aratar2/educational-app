93c5fb70887e867e6beaa648b6b45026cbf761cf
Айрат <aratar5@yandex.ru>
2016-12-24 23:54:51 +0300

    настройка плдкл к бд

--- db_config.php
+++ db_config.php
@@ -2,10 +2,10 @@
  //header("Content-Type: text/html; charset=utf-8");
 
   
-$hostname = "mysql.hostinger.ru"; 
-$username = "u795622068_mybaz"; 
-$password = "zWV7cKKmha"; 
-$dbName = "u795622068_mybaz"; 
+$hostname = "localhost"; 
+$username = "aratar"; 
+$password = "364332"; 
+$dbName = "test"; 
 
 $con=mysql_connect($hostname,$username,$password);
 $db=mysql_select_db($dbName) or die(mysql_error()); 
